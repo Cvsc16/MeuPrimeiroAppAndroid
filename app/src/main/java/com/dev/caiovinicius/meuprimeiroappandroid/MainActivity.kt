965f4ab15c3ba21ccac609e350be33ab2aa424be
Caio Costa <caio.costa@grupocriar.com>
2026-02-03 16:07:42 -0300

    att

--- app/src/main/java/com/dev/caiovinicius/meuprimeiroappandroid/MainActivity.kt
+++ app/src/main/java/com/dev/caiovinicius/meuprimeiroappandroid/MainActivity.kt
@@ -1,20 +1,41 @@
 package com.dev.caiovinicius.meuprimeiroappandroid
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.dev.caiovinicius.meuprimeiroappandroid.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+//        val tvMyFirstAndroidApp = findViewById<TextView>(R.id.tvMyFirstAndroidApp2)
+//        with(tvMyFirstAndroidApp) {  //ERRO!
+//            text = "Meu primeiro App Android!"
+//            textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+//        }
+
+        with(binding.tvMyFirstAndroidApp) {
+            text = "Meu Primeiro App Android!"
+            textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+        }
+
     }
 }
